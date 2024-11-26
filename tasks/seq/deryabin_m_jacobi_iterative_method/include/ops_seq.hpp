4df7213981e65ca9fb87cc2b5147d18c36e50981
BrunoBucCcellati <148016107+BrunoBucCcellati@users.noreply.github.com>
2024-11-26 23:59:51 +0300

    Update ops_seq.hpp

--- tasks/seq/deryabin_m_jacobi_iterative_method/include/ops_seq.hpp
+++ tasks/seq/deryabin_m_jacobi_iterative_method/include/ops_seq.hpp
@@ -1,23 +1,19 @@
-#pragma once
-
-#include "core/task/include/task.hpp"
-
 #include <vector>
 
 namespace deryabin_m_jacobi_iterative_method_seq {
 
 class JacobiIterativeTaskSequential : public ppc::core::Task {
- public:
-  explicit JacobiIterativeTaskSequential(std::shared_ptr<ppc::core::TaskData> taskData_) : Task(std::move(taskData_)) {}
+public:
+  explicit JacobiIterativeTaskSequential(
+      std::shared_ptr<ppc::core::TaskData> taskData_)
+      : Task(std::move(taskData_)) {}
   bool pre_processing() override;
   bool validation() override;
   bool run() override;
   bool post_processing() override;
 
- private:
-  std::vector<double> input_matrix_{}; 
+private:
+  std::vector<double> input_matrix_{};
   std::vector<double> input_right_vector_{};
   std::vector<double> output_x_vector_{};
 };
-
-}  // deryabin_m_jacobi_iterative_method_seq
