982ae47b0db4581e263c2c91825c495231cbf30e
BrunoBucCcellati <148016107+BrunoBucCcellati@users.noreply.github.com>
2024-11-27 09:32:04 +0300

    Update ops_seq.cpp

--- tasks/seq/deryabin_m_jacobi_iterative_method/src/ops_seq.cpp
+++ tasks/seq/deryabin_m_jacobi_iterative_method/src/ops_seq.cpp
@@ -22,7 +22,7 @@ bool deryabin_m_jacobi_iterative_method_seq::JacobiIterativeTaskSequential::vali
   };
   // Check conditions of convergence and applicability of the Jacobi method
   unsigned short i = 0;
-  auto lambda = [](auto first, auto second) { return std::abs(first) + std::abs(second); };
+  double lambda = [&](double first, double second) { return fabs(first) + fabs(second); };
   while (i != sqrt(input_matrix_.size())) {
     if (i == 0) {
       if (fabs(input_matrix_[i * (sqrt(input_matrix_.size()) + 1)]) <=
