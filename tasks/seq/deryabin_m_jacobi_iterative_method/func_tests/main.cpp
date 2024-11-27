ab2c10855d7393cfc1ac105e5b382d5fda6400ac
BrunoBucCcellati <148016107+BrunoBucCcellati@users.noreply.github.com>
2024-11-27 05:03:19 +0300

    Update main.cpp

--- tasks/seq/deryabin_m_jacobi_iterative_method/func_tests/main.cpp
+++ tasks/seq/deryabin_m_jacobi_iterative_method/func_tests/main.cpp
@@ -39,7 +39,7 @@ TEST(deryabin_m_jacobi_iterative_method_seq, test_triangular_matrix) {
                                     0,  0, 0, 28, 13, 14, 0, 0,  0, 0, 16, 15, 0, 0, 0,  0,  0,  17};
   std::vector<double> input_right_vector_{ 86, 202, 269, 261, 170, 102};
   std::vector<double> output_x_vector_ = std::vector<double>(6, 0);
-  std::vector<double> true_solution{ 1, 2, 3, 4, 5, 6}
+  std::vector<double> true_solution{ 1, 2, 3, 4, 5, 6};
 
   std::vector<std::vector<double>> in_matrix(1, input_matrix_);
   std::vector<std::vector<double>> in_right_part(1, input_right_vector_);
@@ -69,7 +69,7 @@ TEST(deryabin_m_jacobi_iterative_method_seq, test_diagonal_elements_are_much_lar
                                     16,  17, 18, 999, 19, 20, 21, 22,  23, 24, 999, 25, 26, 27, 28,  29, 30, 999};
   std::vector<double> input_right_vector_{1069, 2162, 3244, 4315, 5375, 6424};
   std::vector<double> output_x_vector_ = std::vector<double>(6, 0);
-  std::vector<double> true_solution{ 1, 2, 3, 4, 5, 6}
+  std::vector<double> true_solution{ 1, 2, 3, 4, 5, 6};
 
   std::vector<std::vector<double>> in_matrix(1, input_matrix_);
   std::vector<std::vector<double>> in_right_part(1, input_right_vector_);
