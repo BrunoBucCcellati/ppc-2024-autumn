47e08b771cbe0e3971bda2a7d25a1b25ab8d9aae
BrunoBucCcellati <148016107+BrunoBucCcellati@users.noreply.github.com>
2024-11-27 07:53:23 +0300

    Update ops_seq.cpp

--- tasks/seq/deryabin_m_jacobi_iterative_method/src/ops_seq.cpp
+++ tasks/seq/deryabin_m_jacobi_iterative_method/src/ops_seq.cpp
@@ -5,49 +5,65 @@
 #include <thread>
 #include <vector>
 
-bool deryabin_m_jacobi_iterative_method_seq::JacobiIterativeTaskSequential::pre_processing() {
+bool deryabin_m_jacobi_iterative_method_seq::JacobiIterativeTaskSequential::
+    pre_processing() {
   internal_order_test();
   // Init value for input and output
-  input_matrix_ = reinterpret_cast<std::vector<double> *>(taskData->inputs[0])[0];
-  input_right_vector_ = reinterpret_cast<std::vector<double> *>(taskData->inputs[1])[0];
+  input_matrix_ =
+      reinterpret_cast<std::vector<double> *>(taskData->inputs[0])[0];
+  input_right_vector_ =
+      reinterpret_cast<std::vector<double> *>(taskData->inputs[1])[0];
   output_x_vector_ = std::vector<double>(input_right_vector_.size(), double());
   return true;
 }
 
-bool deryabin_m_jacobi_iterative_method_seq::JacobiIterativeTaskSequential::validation() {
+bool deryabin_m_jacobi_iterative_method_seq::JacobiIterativeTaskSequential::
+    validation() {
   internal_order_test();
   // Check count elements
-  if (taskData->inputs_count[0] != 1 || taskData->outputs_count[0] != 1 || taskData->inputs_count[1] != 1) {
+  if (taskData->inputs_count[0] != 1 || taskData->outputs_count[0] != 1 ||
+      taskData->inputs_count[1] != 1) {
     return false;
   };
   // Check conditions of convergence and applicability of the Jacobi method
   unsigned short i = 0;
-  auto lambda = [](auto first, auto second) { return std::abs(first) + std::abs(second); };
+  auto lambda = [](auto first, auto second) {
+    return std::abs(first) + std::abs(second);
+  };
   while (i != sqrt(input_matrix_.size())) {
     if (i == 0) {
       if (fabs(input_matrix_[i * (sqrt(input_matrix_.size()) + 1)]) <=
-	  std::accumulate(
-	    input_matrix_.begin() + 1, input_matrix_.begin() + sqrt(input_matrix_.size()) - 1, 0, lambda) ||
-	input_matrix_[i * (sqrt(input_matrix_.size()) + 1)] == 0) {
-	return false;
+              std::accumulate(input_matrix_.begin() + 1,
+                              input_matrix_.begin() +
+                                  sqrt(input_matrix_.size()) - 1,
+                              0, lambda) ||
+          input_matrix_[i * (sqrt(input_matrix_.size()) + 1)] == 0) {
+        return false;
       }
     }
     if (i > 0 && i < sqrt(input_matrix_.size()) - 1) {
       if (fabs(input_matrix_[i * (sqrt(input_matrix_.size()) + 1)]) <=
-	  std::accumulate(input_matrix_.begin() + i * sqrt(input_matrix_.size()),
-	    input_matrix_.begin() + i * (sqrt(input_matrix_.size()) + 1) - 1, 0, lambda) +
-	    std::accumulate(input_matrix_.begin() + i * (sqrt(input_matrix_.size()) + 1) + 1,
-	      input_matrix_.begin() + (i + 1) * sqrt(input_matrix_.size()) - 1, 0, lambda) ||
-	input_matrix_[i * (sqrt(input_matrix_.size()) + 1)] == 0) {
-	return false;
+              std::accumulate(input_matrix_.begin() +
+                                  i * sqrt(input_matrix_.size()),
+                              input_matrix_.begin() +
+                                  i * (sqrt(input_matrix_.size()) + 1) - 1,
+                              0, lambda) +
+                  std::accumulate(input_matrix_.begin() +
+                                      i * (sqrt(input_matrix_.size()) + 1) + 1,
+                                  input_matrix_.begin() +
+                                      (i + 1) * sqrt(input_matrix_.size()) - 1,
+                                  0, lambda) ||
+          input_matrix_[i * (sqrt(input_matrix_.size()) + 1)] == 0) {
+        return false;
       }
     }
     if (i == sqrt(input_matrix_.size()) - 1) {
       if (fabs(input_matrix_[i * (sqrt(input_matrix_.size()) + 1)]) <=
-	  std::accumulate(
-	    input_matrix_.begin() + i * sqrt(input_matrix_.size()), input_matrix_.end() - 1, 0, lambda) ||
-	input_matrix_[i * (sqrt(input_matrix_.size()) + 1)] == 0) {
-	return false;
+              std::accumulate(input_matrix_.begin() +
+                                  i * sqrt(input_matrix_.size()),
+                              input_matrix_.end() - 1, 0, lambda) ||
+          input_matrix_[i * (sqrt(input_matrix_.size()) + 1)] == 0) {
+        return false;
       }
     }
     i++;
@@ -55,7 +71,8 @@ bool deryabin_m_jacobi_iterative_method_seq::JacobiIterativeTaskSequential::vali
   return true;
 }
 
-bool deryabin_m_jacobi_iterative_method_seq::JacobiIterativeTaskSequential::run() {
+bool deryabin_m_jacobi_iterative_method_seq::JacobiIterativeTaskSequential::
+    run() {
   internal_order_test();
   unsigned short Nmax = 10000, num_of_iterations = 0;
   double epsilon = pow(10, -6), max_delta_x_i = 0;
@@ -68,15 +85,16 @@ bool deryabin_m_jacobi_iterative_method_seq::JacobiIterativeTaskSequential::run(
       j = 0;
       sum = 0;
       while (j != sqrt(input_matrix_.size())) {
-	if (i != j) {
-	  sum += input_matrix_[i * sqrt(input_matrix_.size()) + j] * x_old[j];
-	}
-	j++;
+        if (i != j) {
+          sum += input_matrix_[i * sqrt(input_matrix_.size()) + j] * x_old[j];
+        }
+        j++;
       }
       output_x_vector_[i] =
-	(1.0 / input_matrix_[i * (sqrt(input_matrix_.size()) + 1)]) * (input_right_vector_[i] - sum);
+          (1.0 / input_matrix_[i * (sqrt(input_matrix_.size()) + 1)]) *
+          (input_right_vector_[i] - sum);
       if (fabs(output_x_vector_[i] - x_old[i]) > max_delta_x_i) {
-	max_delta_x_i = fabs(output_x_vector_[i] - x_old[i]);
+        max_delta_x_i = fabs(output_x_vector_[i] - x_old[i]);
       }
       i++;
     }
@@ -85,8 +103,10 @@ bool deryabin_m_jacobi_iterative_method_seq::JacobiIterativeTaskSequential::run(
   return true;
 }
 
-bool deryabin_m_jacobi_iterative_method_seq::JacobiIterativeTaskSequential::post_processing() {
+bool deryabin_m_jacobi_iterative_method_seq::JacobiIterativeTaskSequential::
+    post_processing() {
   internal_order_test();
-  reinterpret_cast<std::vector<double> *>(taskData->outputs[0])[0] = output_x_vector_;
+  reinterpret_cast<std::vector<double> *>(taskData->outputs[0])[0] =
+      output_x_vector_;
   return true;
 }
