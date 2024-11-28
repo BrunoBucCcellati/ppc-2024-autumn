0c55da6981eb275fe117a268ac5ab589a1b76169
BrunoBucCcellati <148016107+BrunoBucCcellati@users.noreply.github.com>
2024-11-28 07:29:10 +0300

    Update main.cpp

--- tasks/seq/deryabin_m_jacobi_iterative_method/perf_tests/main.cpp
+++ tasks/seq/deryabin_m_jacobi_iterative_method/perf_tests/main.cpp
@@ -12,6 +12,7 @@ TEST(deryabin_m_jacobi_iterative_method_seq, test_pipeline_run) {
   std::vector<double> input_right_vector_ = std::vector<double>(100);
   std::vector<double> output_x_vector_ = std::vector<double>(100, 0);
   unsigned short razmernost = 0;
+	
   while (razmernost < 10000) { 
     if (razmernost < 100) {
       input_right_vector_[razmernost] = razmernost + 1;
@@ -39,6 +40,7 @@ TEST(deryabin_m_jacobi_iterative_method_seq, test_pipeline_run) {
   // Create Task
   auto jacobi_iterative_method_TaskSequential =
       std::make_shared<deryabin_m_jacobi_iterative_method_seq::JacobiIterativeTaskSequential>(taskDataSeq);
+
 	
   // Create Perf attributes
   auto perfAttr = std::make_shared<ppc::core::PerfAttr>();
@@ -66,6 +68,7 @@ TEST(deryabin_m_jacobi_iterative_method_seq, test_task_run) {
   std::vector<double> input_right_vector_ = std::vector<double>(100);
   std::vector<double> output_x_vector_ = std::vector<double>(100, 0);
   unsigned short razmernost = 0;
+	
   while (razmernost < 10000) { 
     if (razmernost < 100) {
       input_right_vector_[razmernost] = razmernost + 1;
@@ -93,6 +96,7 @@ TEST(deryabin_m_jacobi_iterative_method_seq, test_task_run) {
   // Create Task
   auto jacobi_iterative_method_TaskSequential =
       std::make_shared<deryabin_m_jacobi_iterative_method_seq::JacobiIterativeTaskSequential>(taskDataSeq);
+
 	
   // Create Perf attributes
   auto perfAttr = std::make_shared<ppc::core::PerfAttr>();
