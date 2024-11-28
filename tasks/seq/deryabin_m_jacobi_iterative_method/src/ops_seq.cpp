4547b9a32196c1f02a9b4ebbce4bcd79cd6cc6ac
BrunoBucCcellati <148016107+BrunoBucCcellati@users.noreply.github.com>
2024-11-28 07:30:50 +0300

    Update ops_seq.cpp

--- tasks/seq/deryabin_m_jacobi_iterative_method/src/ops_seq.cpp
+++ tasks/seq/deryabin_m_jacobi_iterative_method/src/ops_seq.cpp
@@ -30,8 +30,10 @@ bool deryabin_m_jacobi_iterative_method_seq::JacobiIterativeTaskSequential::vali
     }
     if (i > 0 && i < sqrt(matrix.size()) - 1) {
       if (std::abs(matrix[i * (sqrt(matrix.size()) + 1)]) <=
+        
               std::accumulate(matrix.begin() + i * sqrt(matrix.size()), 
                               matrix.begin() + i * (sqrt(matrix.size()) + 1) - 1, 0, Lambda) +
+        
                   std::accumulate(matrix.begin() + i * (sqrt(matrix.size()) + 1) + 1, 
                                   matrix.begin() + (i + 1) * sqrt(matrix.size()) - 1, 0, Lambda) ||
           matrix[i * (sqrt(matrix.size()) + 1)] == 0) {
@@ -72,6 +74,7 @@ bool deryabin_m_jacobi_iterative_method_seq::JacobiIterativeTaskSequential::run(
         }
         j++;
       }
+      
       output_x_vector_[i] = 
           (1.0 / input_matrix_[i * (sqrt(input_matrix_.size()) + 1)]) * (input_right_vector_[i] - sum);
       if (fabs(output_x_vector_[i] - x_old[i]) > max_delta_x_i) {
