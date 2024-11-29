39c6a934f887ff41dc5d9b6b08beae8d51e43aac
BrunoBucCcellati <148016107+BrunoBucCcellati@users.noreply.github.com>
2024-11-29 10:15:44 +0300

    Update ops_seq.cpp

--- tasks/seq/deryabin_m_jacobi_iterative_method/src/ops_seq.cpp
+++ tasks/seq/deryabin_m_jacobi_iterative_method/src/ops_seq.cpp
@@ -19,10 +19,9 @@ bool deryabin_m_jacobi_iterative_method_seq::JacobiIterativeTaskSequential::vali
   auto lambda = [](double first, double second) { return (std::abs(first) + std::abs(second)); };
   while (i != sqrt(input_matrix_.size())) {
     if (i == 0) {
-      if (std::abs(input_matrix_[0]) <=
-              std::accumulate(input_matrix_.begin() + 1, 
-                              input_matrix_.begin() + sqrt(input_matrix_.size()) - 1, 0, 
-                              lambda) ||
+      if (std::abs(input_matrix_[0]) <= std::accumulate(input_matrix_.begin() + 1, 
+                   input_matrix_.begin() + sqrt(input_matrix_.size()) - 1, 0, 
+                   lambda) ||
           input_matrix_[0] == 0) {
         return false;
       }
