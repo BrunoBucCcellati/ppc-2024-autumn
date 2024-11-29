35015f9accdcc1be2f3f18f9e7b059aebddd9bdd
BrunoBucCcellati <148016107+BrunoBucCcellati@users.noreply.github.com>
2024-11-29 11:08:59 +0300

    Update ops_seq.cpp

--- tasks/seq/deryabin_m_jacobi_iterative_method/src/ops_seq.cpp
+++ tasks/seq/deryabin_m_jacobi_iterative_method/src/ops_seq.cpp
@@ -14,7 +14,11 @@ bool deryabin_m_jacobi_iterative_method_seq::JacobiIterativeTaskSequential::pre_
 
 bool deryabin_m_jacobi_iterative_method_seq::JacobiIterativeTaskSequential::validation() {
   internal_order_test();
-  auto* matrix_ = reinterpret_cast<double*>(taskData->inputs[0])[0];
+  matrix_ = std::vector<double>((taskData->inputs[0])[0].size());
+  auto *tmp_matrix_ = reinterpret_cast<double *>(taskData->inputs[0])[0];
+  for (unsigned i = 0; i < matrix_.size(); i++) {
+    matrix_[i] = tmp_matrix_[i];
+  }
   unsigned short i = 0;
   auto lambda = [](double first, double second) { return (std::abs(first) + std::abs(second)); };
   while (i != sqrt(matrix_.size())) {
