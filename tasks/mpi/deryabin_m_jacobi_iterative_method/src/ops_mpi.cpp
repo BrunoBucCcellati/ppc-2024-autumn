8e3808ea64c5dbd7cdb8c50f7c58ec27d666a007
BrunoBucCcellati <148016107+BrunoBucCcellati@users.noreply.github.com>
2024-12-02 19:49:47 +0300

    Update ops_mpi.cpp

--- tasks/mpi/deryabin_m_jacobi_iterative_method/src/ops_mpi.cpp
+++ tasks/mpi/deryabin_m_jacobi_iterative_method/src/ops_mpi.cpp
@@ -168,7 +168,7 @@ bool deryabin_m_jacobi_iterative_method_mpi::JacobiIterativeMPITaskParallel::val
       if (std::abs(loc_matrix_part_[0]) <=
           std::accumulate(loc_matrix_part_.begin() + 1, loc_matrix_part_.begin() + sqrt(taskData->inputs_count[0]) - 1, 
                           0, lambda)) {
-        return false;
+        //return false;
       }
     }
     if (world.rank() == 0) {
@@ -188,7 +188,7 @@ bool deryabin_m_jacobi_iterative_method_mpi::JacobiIterativeMPITaskParallel::val
                 std::accumulate(loc_matrix_part_.begin() + (i + 1) * sqrt(taskData->inputs_count[0]) - 
                                     (number_of_local_matrix_rows + ostatochnoe_chislo_strock - i) + 1, 
                                 loc_matrix_part_.begin() + (i + 1) * sqrt(taskData->inputs_count[0]) - 1, 0, lambda)) {
-          //return false;
+          return false;
         }
       }
     } else {
