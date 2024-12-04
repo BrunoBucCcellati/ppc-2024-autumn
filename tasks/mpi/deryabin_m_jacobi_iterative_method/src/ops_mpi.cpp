291fa15f98c21a2e37f68f953b37a6f59b08119e
BrunoBucCcellati <148016107+BrunoBucCcellati@users.noreply.github.com>
2024-12-05 00:22:52 +0300

    Update ops_mpi.cpp

--- tasks/mpi/deryabin_m_jacobi_iterative_method/src/ops_mpi.cpp
+++ tasks/mpi/deryabin_m_jacobi_iterative_method/src/ops_mpi.cpp
@@ -224,7 +224,7 @@ bool deryabin_m_jacobi_iterative_method_mpi::JacobiIterativeMPITaskParallel::run
       //boost::mpi::broadcast(world, output_x_vector_.data(), number_of_local_matrix_rows, 0);
   } else {
     output_x_vector_[1] = 1;
-    boost::mpi::gatherv(world, output_x_vector_.data() + 1, 1, output_x_vector_.data(), sendcounts, 0);
+    boost::mpi::gatherv(world, local_input_matrix_part_.data(), 1, output_x_vector_.data(), sendcounts, 0);
   }
   
     //if (world.rank() == 0) {
