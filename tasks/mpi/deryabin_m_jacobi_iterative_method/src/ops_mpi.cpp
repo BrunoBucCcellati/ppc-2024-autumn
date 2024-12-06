7449e64f86514aeab8a179da3c2567484d2c0004
BrunoBucCcellati <148016107+BrunoBucCcellati@users.noreply.github.com>
2024-12-06 16:03:12 +0300

    Update ops_mpi.cpp

--- tasks/mpi/deryabin_m_jacobi_iterative_method/src/ops_mpi.cpp
+++ tasks/mpi/deryabin_m_jacobi_iterative_method/src/ops_mpi.cpp
@@ -228,15 +228,16 @@ bool deryabin_m_jacobi_iterative_method_mpi::JacobiIterativeMPITaskParallel::run
     //boost::mpi::gather(world, 2.0, 0);
     //output_x_vector_[1] = 1;
   //}
-  //std::vector<double> v;
+  std::vector<double> v;
   //boost::mpi::gather(world, sendcounts, v, 0);
   //std::vector<int> sendcounts(world.size(), 1);
   //std::vector<int> displacements(world.size(), 0);
   //for (int i = 1; i < world.size(); i++) {
     //displacements[i] = displacements[i - 1] + 1;
   //}
+  boost::mpi::gather(world, 1.0, v, 0);
   //boost::mpi::gatherv(world, local_input_matrix_part_.data() + world.rank(), sendcounts[world.rank()], output_x_vector_.data(), sendcounts, 0);
-  //std::copy(v.begin(), v.end(), output_x_vector_.begin());
+  std::copy(v.begin(), v.end(), output_x_vector_.begin());
     //if (world.rank() == 0) {
       //boost::mpi::broadcast(world, output_x_vector_.data() + number_of_local_matrix_rows, number_of_local_matrix_rows + ostatochnoe_chislo_strock, 0);
     //} else {
@@ -244,7 +245,6 @@ bool deryabin_m_jacobi_iterative_method_mpi::JacobiIterativeMPITaskParallel::run
     //}
   //} while (num_of_iterations < Nmax);
   //boost::mpi::gatherv(world, output_x_vector_.data(), output_x_vector_, output_x_vector_, output_x_vector_, output_x_vector_, 0);
-  boost::mpi::gather(world, 1.0, output_x_vector_, 0);
   return true;
 }
 
