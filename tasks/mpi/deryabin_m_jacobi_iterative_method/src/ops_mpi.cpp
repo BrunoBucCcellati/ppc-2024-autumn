2b744d594f3f59a5bab1d32ae26a662a6bf64faa
BrunoBucCcellati <148016107+BrunoBucCcellati@users.noreply.github.com>
2024-12-07 12:39:35 +0300

    Update ops_mpi.cpp

--- tasks/mpi/deryabin_m_jacobi_iterative_method/src/ops_mpi.cpp
+++ tasks/mpi/deryabin_m_jacobi_iterative_method/src/ops_mpi.cpp
@@ -197,7 +197,7 @@ bool deryabin_m_jacobi_iterative_method_mpi::JacobiIterativeMPITaskParallel::val
 
 bool deryabin_m_jacobi_iterative_method_mpi::JacobiIterativeMPITaskParallel::run() {
   internal_order_test();
-  std::vector<double> vec(world.size(), 1);
+  std::vector<double> vec{1, 2, 3, 4, 5, 6};
   std::vector<double> v(world.size(), 0);
   std::vector<int> sendcounts(world.size(), 1);
   boost::mpi::gatherv(world, vec.data() + world.rank(), 1, v.data(), sendcounts, 0);
