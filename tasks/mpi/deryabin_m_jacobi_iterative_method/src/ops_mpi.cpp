01d7b2e044cd08854e829ed8e629c69f837aaf10
BrunoBucCcellati <148016107+BrunoBucCcellati@users.noreply.github.com>
2024-12-08 23:48:56 +0300

    Update ops_mpi.cpp

--- tasks/mpi/deryabin_m_jacobi_iterative_method/src/ops_mpi.cpp
+++ tasks/mpi/deryabin_m_jacobi_iterative_method/src/ops_mpi.cpp
@@ -245,12 +245,12 @@ bool deryabin_m_jacobi_iterative_method_mpi::JacobiIterativeMPITaskParallel::run
       }
       i++;
     }
-    //if (world.rank() == 0) {
-      //boost::mpi::gatherv(world, local_output_x_vector_part_.data(), number_of_local_matrix_rows + ostatochnoe_chislo_strock, output_x_vector_.data(), sendcounts, 0);
-    //} else {
-      //boost::mpi::gatherv(world, local_output_x_vector_part_.data(), number_of_local_matrix_rows, output_x_vector_.data(), sendcounts, 0);
-    //}
-    //boost::mpi::broadcast(world, output_x_vector_.data(), output_x_vector_.size(), 0);
+    if (world.rank() == 0) {
+      boost::mpi::gatherv(world, local_output_x_vector_part_.data(), number_of_local_matrix_rows + ostatochnoe_chislo_strock, output_x_vector_.data(), sendcounts, 0);
+    } else {
+      boost::mpi::gatherv(world, local_output_x_vector_part_.data(), number_of_local_matrix_rows, output_x_vector_.data(), sendcounts, 0);
+    }
+    boost::mpi::broadcast(world, output_x_vector_.data(), output_x_vector_.size(), 0);
     num_of_iterations++;
   } while (num_of_iterations < Nmax && max_delta_x_i > epsilon);
   return true;
