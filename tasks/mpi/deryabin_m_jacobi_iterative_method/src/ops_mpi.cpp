4a39d5b33b1c4d6e54d9e75e245736df063cda8c
BrunoBucCcellati <148016107+BrunoBucCcellati@users.noreply.github.com>
2024-12-08 17:47:44 +0300

    Update ops_mpi.cpp

--- tasks/mpi/deryabin_m_jacobi_iterative_method/src/ops_mpi.cpp
+++ tasks/mpi/deryabin_m_jacobi_iterative_method/src/ops_mpi.cpp
@@ -202,7 +202,7 @@ bool deryabin_m_jacobi_iterative_method_mpi::JacobiIterativeMPITaskParallel::run
     //sendcounts[world.rank()] = number_of_local_matrix_rows + ostatochnoe_chislo_strock;
     //for (int proc = 1; proc < world.size(); proc++) {
       //world.send(proc, 0, sendcounts.data() + proc, number_of_local_matrix_rows);
-    }
+    //}
   }
   boost::mpi::broadcast(world, number_of_local_matrix_rows, 0);
   if (world.rank() != 0) {
